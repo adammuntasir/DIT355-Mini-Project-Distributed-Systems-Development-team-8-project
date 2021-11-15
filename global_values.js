638efeaa1cb0d223c65656800edbbf6d5d376ef6
sramzi <sramzi@student.chalmers.se>
2021-11-15 17:50:36 +0100

    add the values that allow connecting to the broker and the topics

---
+++ global_values.js
@@ -0,0 +1,12 @@
+const global_values = {
+    ip_address: 'broker.hivemq.com',
+    port: '8000',
+
+    sendToClient: 'ramzimkoqaz1984qwe',
+    FromClient: 'ramzimkoqaz1984qwe2',
+    toExtractData: 'ramzimkoqaz1984qwe3',
+    toRegistry: 'ramzimkoqaz1984qwe4'
+
+}
+
+module.exports = global_values
