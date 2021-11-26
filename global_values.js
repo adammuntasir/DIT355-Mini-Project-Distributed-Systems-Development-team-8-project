8ca057c72c704cfd9eec4b9b80c902a7df5ddfec
sramzi <sramzi@student.chalmers.se>
2021-11-26 04:49:44 +0100

    changed topics

--- global_values.js
+++ global_values.js
@@ -2,10 +2,10 @@ const global_values = {
     ip_address: 'broker.hivemq.com',
     port: '8000',
 
-    sendToClient: 'ramzimkoqaz1984qwe',
-    FromClient: 'ramzimkoqaz1984qwe2',
-    toExtractData: 'ramzimkoqaz1984qwe3',
-    toRegistry: 'ramzimkoqaz1984qwe4'
+    sendToClient: 'ramzimkoqaz1984qwe1',
+    FromClient: 'ramzimkoqaz1984qwe22',
+    toExtractData: 'ramzimkoqaz1984qwe33',
+    toRegistry: 'ramzimkoqaz1984qwe44'
 
 }
 
