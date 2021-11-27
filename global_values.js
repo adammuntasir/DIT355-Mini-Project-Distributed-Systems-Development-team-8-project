2032eada0214a0d1ab38abfeb5442904c06067fc
sramzi <sramzi@student.chalmers.se>
2021-11-27 13:03:26 +0100

    added new topic for new component

--- global_values.js
+++ global_values.js
@@ -5,7 +5,8 @@ const global_values = {
     sendToClient: 'ramzimkoqaz1984qwe1',
     FromClient: 'ramzimkoqaz1984qwe22',
     toExtractData: 'ramzimkoqaz1984qwe33',
-    toRegistry: 'ramzimkoqaz1984qwe44'
+    sendToDataHandler: 'ramzimkoqaz1984qwe44',
+    sendToExtractData: 'ramzimkoqaz1984qwe55'
 
 }
 
