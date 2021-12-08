8c5a0765433ec4ae0d3cbc0da198c387299f01cc
sramzi <sramzi@student.chalmers.se>
2021-12-08 09:04:48 +0100

    updated the global values

--- global_values.js
+++ global_values.js
@@ -3,11 +3,12 @@ const global_values = {
     port: '8000',
 
     sendToClient: 'ramzimkoqaz1984qwe1',
-    FromClient: 'ramzimkoqaz1984qwe22',
+    FromClient: 'ramzimkoqaz1984qwe22', // we will use this for now
     toExtractData: 'ramzimkoqaz1984qwe33',
     sendToDataHandler: 'ramzimkoqaz1984qwe44',
-    sendToExtractData: 'ramzimkoqaz1984qwe55'
-
+    sendToExtractData: 'ramzimkoqaz1984qwe55',
+    sendToCircuitBreaker: 'ramzimkoqaz1984qwe66', // we are waiting for request generator
+    sendToFormatChecker: 'ramzimkoqaz1984qwe77'
 }
 
 module.exports = global_values
